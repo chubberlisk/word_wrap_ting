daacca2ef1ba230a354a2376ea587383eee804af
Wen Ting Wang <chubberlisk@outlook.com>
2019-02-18 14:13:58 +0000

    test: add tests for wrap method

--- spec/word_wrap_ting_spec.rb
+++ spec/word_wrap_ting_spec.rb
@@ -5,7 +5,39 @@ RSpec.describe WordWrapTing do
     expect(WordWrapTing::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'returns an empty string when called with nothing' do
+    expect(WordWrapTing.wrap('', 1)).to eq('')
+  end
+
+  it 'returns the word if equal to the max length' do
+    expect(WordWrapTing.wrap('word', 4)).to eq('word')
+  end
+
+  it 'returns the word if shorter than the max length' do
+    expect(WordWrapTing.wrap('word', 5)).to eq('word')
+  end
+
+  it 'breaks up a long word with a newline' do
+    expect(WordWrapTing.wrap('longword', 4)).to eq("long\nword")
+  end
+
+  it 'breaks up a very long word with two newlines' do
+    expect(WordWrapTing.wrap('verylongword', 4)).to eq("very\nlong\nword")
+  end
+
+  it 'wraps before word boundary' do
+    expect(WordWrapTing.wrap('long word', 4)).to eq("long\nword")
+  end
+
+  it 'wraps on word boundary' do
+    expect(WordWrapTing.wrap('long word', 5)).to eq("long\nword")
+  end
+
+  it 'wraps after word boundary' do
+    expect(WordWrapTing.wrap('long word', 6)).to eq("long\nword")
+  end
+
+  it 'keeps as many words as possible on the line' do
+    expect(WordWrapTing.wrap('very long word', 10)).to eq("very long\nword")
   end
 end
