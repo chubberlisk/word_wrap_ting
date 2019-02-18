2391c1aa38b1a04abbe6ae032e335b30efff7419
Wen Ting Wang <chubberlisk@outlook.com>
2019-02-18 14:14:59 +0000

    feat: wrap strings

--- lib/word_wrap_ting.rb
+++ lib/word_wrap_ting.rb
@@ -4,5 +4,11 @@ require 'word_wrap_ting/version'
 
 module WordWrapTing
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.wrap(str, max_len)
+    return str if str.length <= max_len
+
+    break_col = str[0...max_len].rindex(' ') || max_len
+    str[0...break_col].strip << "\n" << self.wrap(str[break_col..-1].strip, max_len)
+  end
 end
